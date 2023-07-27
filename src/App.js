f6186f46b5bcfb0f1631f252c5c439847b2d8482
Akaash Kolluri <arkolluri@gmail.com>
2023-07-27 16:31:53 -0500

    update

--- src/App.js
+++ src/App.js
@@ -1,19 +1,50 @@
 import logo from "./logo.svg";
 import "./App.css";
+// import "antd/dist/antd.css";
+import "antd/dist/reset.css";
 import axios from "axios";
+import { Input, message, Button, Row } from "antd";
 
 import { useState, useEffect } from "react";
 
 function App() {
+  const [messageApi, contextHolder] = message.useMessage();
   const [options, setOptions] = useState("Loading...");
+  const [page, setPage] = useState(0);
+
+  const [adverb, setAdverb] = useState("");
+  const [adjective, setAdjective] = useState("");
+
   useEffect(() => {
-    getAPI();
+    // getAPI("madly mean");
+    // handleChange = handleChange.bind(this);
   }, []);
-  const getAPI = async () => {
+
+  const handleChange = (event) => {
+    if (event.target.value.indexOf(" ") > -1) {
+      message.open({
+        type: "error",
+        content: "Only one adverb at a time!",
+      });
+    } else setAdverb(event.target.value);
+  };
+
+  const handleChange2 = (event) => {
+    if (event.target.value.indexOf(" ") > -1) {
+      message.open({
+        type: "error",
+        content: "Only one adjective at a time!",
+      });
+    } else setAdjective(event.target.value);
+  };
+  const handleSubmit = () => {
+    getAPI(adverb + " " + adjective);
+  };
+  const getAPI = async (value) => {
     try {
       const result = await axios.get(
         "https://to-hell-with-adverbs-api-git-master-akaash.vercel.app/get?query=" +
-          "extremeley mad"
+          value
       );
 
       setOptions(result.data.result.split(","));
@@ -25,18 +56,34 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload. {options}
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <textarea value={adverb} onChange={handleChange}>
+          {" "}
+        </textarea>
+        <h1> + </h1>
+        <textarea value={adjective} onChange={handleChange2}>
+          {" "}
+        </textarea>
+        <h1> = </h1>
+        <Row>
+          <h1
+            onClick={() => {
+              setPage((page + 1) % 5);
+            }}
+          >
+            {" "}
+            {"< "}{" "}
+          </h1>
+          <h1> {options[page]} </h1>
+          <h1
+            onClick={() => {
+              setPage((page + 4) % 5);
+            }}
+          >
+            {" "}
+            {" >"}{" "}
+          </h1>
+        </Row>
+        <Button onClick={handleSubmit}>Generate</Button>
       </header>
     </div>
   );
